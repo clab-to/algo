b6f6eb26663d9df30906f119a43dec651286c4b8
clab-to <73862233+clab-to@users.noreply.github.com>
2025-02-08 01:47:25 +0900

    add insertion

--- src/sort/InsertionSort.ts
+++ src/sort/InsertionSort.ts
@@ -1,7 +1,38 @@
 // 挿入ソート
 // 各要素分の移動処理が各要素のループ分実施されるのでO(n^2)
 
+import { SortArgoTarget } from "./common/sortArgoTarget";
+
 const intersectionSort = (list: number[]): number[] => {
   const sortList = structuredClone(list);
-  return [];
+
+  const boundary = 1;
+
+  const sortTargetNumber: { [name: string]: number | null } = {
+    index: null,
+    number: null,
+  };
+
+  for (let index = boundary; index < sortList.length; index++) {
+    [sortTargetNumber.index, sortTargetNumber.number] = [
+      index,
+      sortList[index],
+    ];
+
+    let sortedElementIdx = sortTargetNumber.index - 1;
+    while (
+      sortedElementIdx >= 0 &&
+      sortTargetNumber.number < sortList[sortedElementIdx]
+    ) {
+      sortList[sortedElementIdx + 1] = sortList[sortedElementIdx];
+      sortedElementIdx--;
+    }
+    sortList[sortedElementIdx + 1] = sortTargetNumber.number;
+    console.log(sortList);
+  }
+
+  return sortList;
 };
+
+const target = new SortArgoTarget(30);
+target.sort(intersectionSort);
