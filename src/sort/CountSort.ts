a84157c340038be88ffde521b9f455482691032a
clab-to <73862233+clab-to@users.noreply.github.com>
2025-02-08 23:01:33 +0900

    add templete

--- src/sort/CountSort.ts
+++ src/sort/CountSort.ts
@@ -1,4 +1,9 @@
+import { SortArgoTarget } from "./common/sortArgoTarget";
+
 const countSort = (list: number[]): number[] => {
   const sortList = structuredClone(list);
   return [];
 };
+
+const target = new SortArgoTarget(30);
+target.sort(countSort);
--- src/sort/RadixSort.ts
+++ src/sort/RadixSort.ts
@@ -1,4 +1,9 @@
+import { SortArgoTarget } from "./common/sortArgoTarget";
+
 const radixSort = (list: number[]): number[] => {
   const sortList = structuredClone(list);
   return [];
 };
+
+const target = new SortArgoTarget(30);
+target.sort(radixSort);
