5eaf43ff4ce168f475cf3da068a4c6044cc9bce7
clab-to <73862233+clab-to@users.noreply.github.com>
2025-02-09 09:35:12 +0900

    add count

--- src/sort/CountSort.ts
+++ src/sort/CountSort.ts
@@ -2,7 +2,35 @@ import { SortArgoTarget } from "./common/sortArgoTarget";
 
 const countSort = (list: number[]): number[] => {
   const sortList = structuredClone(list);
-  return [];
+
+  const countingList: number[] = [...new Array(Math.max(...sortList))].map(
+    (_) => 0,
+  );
+  const positionByNumberList: number[] = new Array(Math.max(...sortList));
+
+  for (const number of sortList) {
+    countingList[number - 1]++;
+  }
+
+  for (let index = 0; index < positionByNumberList.length; index++) {
+    if (index === 0) {
+      positionByNumberList[index] = countingList[index];
+    } else {
+      positionByNumberList[index] =
+        countingList[index] + positionByNumberList[index - 1];
+    }
+  }
+
+  const resultList: number[] = new Array(Math.max(...sortList));
+  for (let index = 0; index < sortList.length; index++) {
+    const number = sortList[index];
+    resultList[positionByNumberList[number - 1] - 1] = number;
+    positionByNumberList[number - 1]--;
+  }
+
+  console.log(resultList);
+
+  return resultList;
 };
 
 const target = new SortArgoTarget(30);
